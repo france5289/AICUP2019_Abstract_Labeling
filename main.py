d4f5069a2f80c01152ab4b819b35cdcaf65ed894
france5289 <st980407@gmail.com>
2019-11-14 13:20:02 +0800

    move F1 object to model.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import matplotlib.pyplot as plt
 import pickle, re
 import os
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
@@ -21,37 +19,11 @@ from tqdm import trange
 
 from gensim.parsing import remove_stopwords
 
-from model import * # import all model object
+from model import Net1, F1 # import all model object
 from config_writer import write_config
 
 
-class F1():
-    def __init__(self):
-        self.threshold = 0.5
-        self.n_precision = 0
-        self.n_recall = 0
-        self.n_corrects = 0
-        self.name = 'F1'
 
-    def reset(self):
-        self.n_precision = 0
-        self.n_recall = 0
-        self.n_corrects = 0
-
-    def update(self, predicts, groundTruth):
-        predicts = predicts > self.threshold
-        self.n_precision += torch.sum(predicts).data.item()
-        self.n_recall += torch.sum(groundTruth).data.item()
-        self.n_corrects += torch.sum(groundTruth.type(torch.bool) * predicts).data.item()
-
-    def get_score(self):
-        recall = self.n_corrects / self.n_recall
-        precision = self.n_corrects / (self.n_precision + 1e-20)
-        return 2 * (recall * precision) / (recall + precision + 1e-20)
-
-    def print_score(self):
-        score = self.get_score()
-        return '{:.5f}'.format(score)
 
 if __name__ == '__main__':
     CWD = os.getcwd()
--- model.py
+++ model.py
@@ -58,3 +58,34 @@ class Net1(nn.Module):
         x = torch.max(x,dim=2)[0]
         y = self.FCLayer(x)
         return y
+
+class F1():
+    '''
+    This object provide some method to evaluate F1 score 
+    '''
+    def __init__(self):
+        self.threshold = 0.5
+        self.n_precision = 0
+        self.n_recall = 0
+        self.n_corrects = 0
+        self.name = 'F1'
+
+    def reset(self):
+        self.n_precision = 0
+        self.n_recall = 0
+        self.n_corrects = 0
+
+    def update(self, predicts, groundTruth):
+        predicts = predicts > self.threshold
+        self.n_precision += torch.sum(predicts).data.item()
+        self.n_recall += torch.sum(groundTruth).data.item()
+        self.n_corrects += torch.sum(groundTruth.type(torch.bool) * predicts).data.item()
+
+    def get_score(self):
+        recall = self.n_corrects / self.n_recall
+        precision = self.n_corrects / (self.n_precision + 1e-20)
+        return 2 * (recall * precision) / (recall + precision + 1e-20)
+
+    def print_score(self):
+        score = self.get_score()
+        return '{:.5f}'.format(score)
