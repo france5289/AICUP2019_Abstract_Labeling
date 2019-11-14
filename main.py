07bc954c3210035b39a8b126e84445efdcc5fc49
france5289 <st980407@gmail.com>
2019-11-14 17:50:01 +0800

    define AbstractDataset object, import helper func

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 Run this file to train, valid and test model
 '''
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import pickle, re
+import re
 import os
 
-
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
@@ -21,6 +20,47 @@ from gensim.parsing import remove_stopwords
 
 from model import Net1, F1 # import all model object
 from config_writer import write_config
+from DataPreprocessor import Download_Glove, Create_Vocabulary, Create_Glove_embedding_matrix, Get_dataset
+
+
+
+class AbstractDataset(Dataset):
+    def __init__(self, data, pad_idx, max_len = 64):
+        self.data = data
+        self.pad_idx = pad_idx
+        self.max_len = max_len
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+        
+    def collate_fn(self, datas):
+        # get max length in this batch
+        max_sent = max([len(data['Abstract']) for data in datas])
+        max_len = max([min(len(sentence), self.max_len) for data in datas for sentence in data['Abstract']])
+        batch_abstract = []
+        batch_label = []
+        sent_len = []
+        for data in datas:
+            # padding abstract to make them in same length
+            pad_abstract = []
+            for sentence in data['Abstract']:
+                if len(sentence) > max_len:
+                    pad_abstract.append(sentence[:max_len])
+                else:
+                    pad_abstract.append(sentence+[self.pad_idx]*(max_len-len(sentence)))
+            sent_len.append(len(pad_abstract))
+            pad_abstract.extend([[self.pad_idx]*max_len]*(max_sent-len(pad_abstract)))
+            batch_abstract.append(pad_abstract)
+            # gather labels
+            if 'Label' in data:
+                pad_label = data['Label']
+                pad_label.extend([[0]*6]*(max_sent-len(pad_label)))
+                
+                batch_label.append(pad_label)
+        return torch.LongTensor(batch_abstract), torch.FloatTensor(batch_label), sent_len
 
 
 
