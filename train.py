2739e3b468c7facf959249dca0bd2cfdedeb9bc6
france5289 <st980407@gmail.com>
2019-11-19 19:34:24 +0800

    init train.py

---
+++ train.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
+from tqdm import trange
+
+from model import Net1, F1
+from DataPreprocessor import Download_Glove, Create_Glove_embedding_matrix
+
+# set device
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# data path and cpu num
+CWD = os.getcwd()
+TRAIN_DATA_PATH = os.path.join(CWD, 'data', 'trainset.csv')
+VALID_DATA_PATH = os.path.join(CWD, 'data', 'validset.csv')
+TEST_DATA_PATH = os.path.join(CWD, 'data', 'testset.csv')
+WORKERS = os.cpu_count() // 2
+
+
+if __name__ == '__main__':
+
+
+    
