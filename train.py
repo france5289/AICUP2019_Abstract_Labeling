ffb37fc7bd472b58cd7be3ed27a90f7a00cce836
france5289 <st980407@gmail.com>
2019-11-20 13:12:50 +0800

    encode abstract and convert label to one_hot

--- train.py
+++ train.py
@@ -43,6 +43,45 @@ def GenDict(train, valid):
             Tokenizer.build_dict(item)
         Tokenizer.save_to_file(DICT_PATH)
 
+def labels_to_onehot(labels):
+    '''
+    Convert labels to one-hot encoding
+
+    Args : 
+        labels:( DataFrame column item ) 
+    Return :
+        one_hot_labels: ( DataFrame column item )
+    '''
+    one_hot_labels = []
+    label_list = labels.split(' ')
+    label_dict = {'BACKGROUND': 0, 'OBJECTIVES':1, 'METHODS':2, 'RESULTS':3, 'CONCLUSIONS':4, 'OTHERS':5}
+    for label in label_list:
+        onehot = [0,0,0,0,0,0]
+        for l in label.split('/'):
+            onehot[label_dict[l]] = 1
+        one_hot_labels.append(onehot)
+    
+    return one_hot_labels
+
+def encode_data(dataset):
+    '''
+    encode 'Abstract' and convert label to one_hot
+
+
+    Args:
+        dataset(pd.DataFrame)
+    Return:
+        abstract(pd.Series)
+        labels(pd.Series) : if dataset dosen't contain label, then return None 
+    '''
+    global Tokenizer
+    abstract = dataset['Abstract'].apply(func=Tokenizer.encode)
+    labels = None
+    if 'Task 1' in dataset.columns:
+        labels = dataset['Task 1'].apply(func=labels_to_onehot)
+
+    return abstract, labels
+
 
 if __name__ == '__main__':
     if not os.path.exists(TRAIN_DATA_PATH) or not os.path.exists(VALID_DATA_PATH) or not os.path.exists(TEST_DATA_PATH):
@@ -54,4 +93,10 @@ if __name__ == '__main__':
 
     train['Abstract'] = train['Abstract'].apply(func=SplitSent)
     valid['Abstract'] = valid['Abstract'].apply(func=SplitSent)
+    test['Abstract'] = test['Abstract'].apply(func=SplitSent)
     GenDict(train, valid)
+
+    # encode 'Abstract' and convert label to one_hot
+    train_encode, train_label = encode_data(train)
+    valid_encode, valid_label = encode_data(valid)
+    test_encode, _ = encode_data(test)
