f627e1715e61a0c82706d0d36b74d13a911557fb
france5289 <st980407@gmail.com>
2019-11-21 22:48:38 +0800

    implement GRUNet.forward()

--- model.py
+++ model.py
@@ -8,41 +8,58 @@ from collections import OrderedDict
 
 
 class GRUNet(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, layer_num=1, drop_pb=0.5, bidirect=False):
+    def __init__(self,
+                 vocab_size,
+                 embedding_dim,
+                 hidden_dim,
+                 layer_num=1,
+                 drop_pb=0.5,
+                 bidirect=False):
         super(GRUNet, self).__init__()
         GRU_drop_pb = drop_pb
         if layer_num == 1:
             GRU_drop_pb = 0
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.sent_rnn = nn.GRU(embedding_dim, hidden_dim, num_layers=layer_num,
-                               dropout=GRU_drop_pb, bidirectional=bidirect, batch_first=True)
-        self.FCLayer = nn.Sequential(OrderedDict([
-            ('FC1', nn.Linear(hidden_dim*2, hidden_dim)),
-            ('DropOut1', nn.Dropout(drop_pb)),
-            ('ReLU1', nn.ReLU()),
-            ('FC2', nn.Linear(hidden_dim, hidden_dim // 2)),
-            ('DropOut2', nn.Dropout(drop_pb)),
-            ('ReLU2', nn.ReLU()),
-            ('FC3', nn.Linear(hidden_dim // 2, 6)),
-            ('Sigmoid', nn.Sigmoid())
-        ]))
+        self.sent_rnn = nn.GRU(embedding_dim,
+                               hidden_dim,
+                               num_layers=layer_num,
+                               dropout=GRU_drop_pb,
+                               bidirectional=bidirect,
+                               batch_first=True)
+        self.FCLayer = nn.Sequential(
+            OrderedDict([('FC1', nn.Linear(hidden_dim * 2, hidden_dim)),
+                         ('DropOut1', nn.Dropout(drop_pb)),
+                         ('ReLU1', nn.ReLU()),
+                         ('FC2', nn.Linear(hidden_dim, hidden_dim // 2)),
+                         ('DropOut2', nn.Dropout(drop_pb)),
+                         ('ReLU2', nn.ReLU()),
+                         ('FC3', nn.Linear(hidden_dim // 2, 6)),
+                         ('Sigmoid', nn.Sigmoid())]))
         torch.nn.init.xavier_normal_(self.FCLayer[0].weight)
         torch.nn.init.xavier_normal_(self.FCLayer[3].weight)
         torch.nn.init.xavier_normal_(self.FCLayer[6].weight)
 
     def forward(self, x, eos_indexes):
-        pass
-
-
-
+        '''
+        Args:
+            x(Tensor): input tensor with shape b*s
+            eos_indexes(list): list which record positions of every eos tokens
+        '''
+        x = self.embedding(x)
+        x = self.sent_rnn(x)
+        __, _, h = x.size()
+        x = x.view(-1, h)  # (b*s)*(hidden_dim * direction_num)
+        indices = torch.tensor(eos_indexes)
+        x = torch.index_select(x, 0, indices)
+        y = self.FCLayer(x)
+        return y
 
 
 class F1():
     '''
     This object provide some method to evaluate F1 score 
     '''
-
     def __init__(self):
         self.threshold = 0.5
         self.n_precision = 0
@@ -59,8 +76,8 @@ class F1():
         predicts = predicts > self.threshold
         self.n_precision += torch.sum(predicts).data.item()
         self.n_recall += torch.sum(groundTruth).data.item()
-        self.n_corrects += torch.sum(groundTruth.type(torch.bool)
-                                     * predicts).data.item()
+        self.n_corrects += torch.sum(groundTruth.type(torch.bool) *
+                                     predicts).data.item()
 
     def get_score(self):
         recall = self.n_corrects / self.n_recall
