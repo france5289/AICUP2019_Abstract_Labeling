0ed4fd8c6287d2a25c01a93cdcfcc768f79aff98
france5289 <st980407@gmail.com>
2019-11-26 14:31:48 +0800

    NLTKTokenizer override build_dict()

--- tokenizer.py
+++ tokenizer.py
@@ -177,8 +177,6 @@ class BaseTokenizer:
 
         return self
 
-    def get_token_to_id(self):
-        return self.token_to_id
 
 class CharTokenizer(BaseTokenizer):
     def __init__(self, **kwargs):
@@ -203,9 +201,37 @@ class WhiteSpaceTokenizer(BaseTokenizer):
 class NLTKTokenizer(BaseTokenizer):
     def __init__(self, **kwargs):
         super(NLTKTokenizer, self).__init__(**kwargs)
-    
+        self._token_counter = {}
+
+    @property
+    def token_counter(self):
+        return self._token_counter
+
+
     def tokenize(self, sentence):
         return word_tokenize(sentence)
     
     def detokenize(self, tokens):
         return ' '.join(tokens)
+    
+    def get_token_to_id(self):
+        return self.token_to_id
+
+    def build_dict(self, all_sentences, min_count=0):
+        all_sentences = self.convert_sentences_to_tokens(all_sentences)
+        for sentence in all_sentences:
+            for token in sentence:
+                if token in self.token_to_id:
+                    continue
+                if token not in self._token_counter:
+                    self._token_counter[token] = 0
+                self._token_counter[token] += 1
+
+        index = len(self.token_to_id)
+
+        for token, count in self._token_counter.items():
+            if count > min_count:
+                self.token_to_id[token] = index
+                self.id_to_token[index] = token
+                index += 1
+        return self
