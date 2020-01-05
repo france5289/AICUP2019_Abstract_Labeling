5065f6d168ab69069d61ff2c9c3502c226e3aba1
france5289 <st980407@gmail.com>
2020-01-05 15:36:01 +0800

    init inference.py

---
+++ inference.py
@@ -0,0 +1,10 @@
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm, trange
+
+if __name__ == "__main__":
+    pass
