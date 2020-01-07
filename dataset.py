fb26cf81ad7c895b644166bab511f4f3bf59a06b
france5289 <st980407@gmail.com>
2020-01-07 15:44:21 +0800

    init dataset.py metrics.py

---
+++ dataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class AbstractDataset(Dataset):
+    def __init__(self, data, pad_idx, max_len = 64):
+        self.data = data
+        self.pad_idx = pad_idx
+        self.max_len = max_len
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+        
+    def collate_fn(self, datas):
+        # get max length in this batch
+        max_sent = max([len(data['Abstract']) for data in datas])
+        max_len = max([min(len(sentence), self.max_len) for data in datas for sentence in data['Abstract']])
+        batch_abstract = []
+        batch_label = []
+        sent_len = []
+        for data in datas:
+            # padding abstract to make them in same length
+            pad_abstract = []
+            for sentence in data['Abstract']:
+                if len(sentence) > max_len:
+                    pad_abstract.append(sentence[:max_len])
+                else:
+                    pad_abstract.append(sentence+[self.pad_idx]*(max_len-len(sentence)))
+            sent_len.append(len(pad_abstract))
+            pad_abstract.extend([[self.pad_idx]*max_len]*(max_sent-len(pad_abstract)))
+            batch_abstract.append(pad_abstract)
+            # gather labels
+            if 'Label' in data:
+                pad_label = data['Label']
+                pad_label.extend([[0]*6]*(max_sent-len(pad_label)))
+                
+                batch_label.append(pad_label)
+        return torch.LongTensor(batch_abstract), torch.FloatTensor(batch_label), sent_len
